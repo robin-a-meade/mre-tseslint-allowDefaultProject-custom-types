f4eb22c5e86367bd29e77eaa6fc4f1bc576028ca
Robin A. Meade <robin.a.meade@gmail.com>
2024-08-04 07:08:57 -1000

    Fix linting error

--- typings/eslint-plugin-react.d.ts
+++ typings/eslint-plugin-react.d.ts
@@ -10,6 +10,12 @@
 // CHANGELOG
 // * Sat Aug 3 2024 05:53:53 PM HST  Robin A. Meade <robin.a.meade@gmail.com>
 // - added the flat configs that became available in v7.35.0 (Jul 19, 2024)
+// * Sun Aug 4 2024 06:59:24 AM HST  Robin A. Meade <robin.a.meade@gmail.com>
+// - Fix linting error:
+//   "This is the default value for this type parameter, so it can be omitted.
+//    eslint@typescript-eslint/no-unnecessary-type-arguments"
+//   Changed `Linter.Config<Linter.RulesRecord>` to `Linter.Config`
+//   three times.
 
 declare module 'eslint-plugin-react' {
   import type { ESLint, Linter } from 'eslint';
@@ -21,9 +27,9 @@ declare module 'eslint-plugin-react' {
       all: ESLint.ConfigData;
       'jsx-runtime': ESLint.ConfigData;
       flat: {
-        recommended: Linter.Config<Linter.RulesRecord>;
-        all: Linter.Config<Linter.RulesRecord>;
-        'jsx-runtime': Linter.Config<Linter.RulesRecord>;
+        recommended: Linter.Config;
+        all: Linter.Config;
+        'jsx-runtime': Linter.Config;
       };
     };
   };
